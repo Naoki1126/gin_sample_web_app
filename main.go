17834935b62f5e7d39756adac01d3924f9bad1a1
Naoki Maruyama <m.naoki85@gmail.com>
2022-05-25 22:22:35 +0900

    [test] : swaggerで作成されたclientコードのテスト

--- main.go
+++ main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"gin_sample_web_app/config"
-	"gin_sample_web_app/modules"
+	"gin_sample_web_app/swagger/client/operations"
+	"gin_sample_web_app/swagger/models"
+	"log"
+	"time"
+
+	apiclient "gin_sample_web_app/swagger/client"
+
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
 )
 
 func main() {
 	// fmt.Println(111)
 
-	testFuncStruct := modules.TestFunc{}
-	fmt.Println(context.TODO())
-	fmt.Print(testFuncStruct.TestRun(111))
+	// testFuncStruct := modules.TestFunc{}
+	// fmt.Println(context.TODO())
+	// fmt.Print(testFuncStruct.TestRun(111))
 
-	r := config.DefineRoutes()
-	r.Run(":8080")
+	// r := config.DefineRoutes()
+	// r.Run(":8080")
+
+	p := &operations.PostUserParams{
+		Body: &models.User{
+			ID:   1,
+			Name: "name_log",
+		},
+	}
+
+	p.SetTimeout(10 * time.Second)
+
+	transport := httptransport.New("localhost:8000", "api", nil)
+	client := apiclient.New(transport, strfmt.Default)
+
+	res, _ := client.Operations.PostUser(p)
+
+	log.Printf("&#v\n", res.Error())
 }
